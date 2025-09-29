181aceb8603bf4a49b7ccc53df19316755cd2c69
yahya <19204398+yhassanzadeh13@users.noreply.github.com>
2025-09-29 14:03:14 -0700

    Improves worker pool component lifecycle
    Refactors the worker pool to improve its lifecycle management.
    
    Simplifies worker shutdown logging by consolidating messages,
    making the output more concise and readable.

--- modules/worker/pool.go
+++ modules/worker/pool.go
@@ -52,12 +52,16 @@ func NewWorkerPool(logger zerolog.Logger, queueSize int, workerCount int) *Pool 
 
 	p.Manager = component.NewManager(
 		logger,
-		component.WithStartupLogic(func(ctx modules.ThrowableContext) {
-			p.startWorkers(ctx)
-		}),
-		component.WithShutdownLogic(func() {
-			p.stopWorkers()
-		}),
+		component.WithStartupLogic(
+			func(ctx modules.ThrowableContext) {
+				p.startWorkers(ctx)
+			},
+		),
+		component.WithShutdownLogic(
+			func() {
+				p.stopWorkers()
+			},
+		),
 	)
 
 	return p
@@ -151,10 +155,7 @@ func (p *Pool) worker(ctx modules.ThrowableContext, id int) {
 		case <-ctx.Done():
 			p.logger.Trace().
 				Int("worker_id", id).
-				Msg("Worker received context done signal")
-			p.logger.Trace().
-				Int("worker_id", id).
-				Msg("Worker shutting down")
+				Msg("Worker received context done signal, and is shutting down")
 			return
 		case job, ok := <-p.queue:
 			if !ok {
